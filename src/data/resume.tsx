23608d6a75bc6ef01a30e2a6d60dd4ced8f406e5
monsterchick <kwanho.app@gmail.com>
2025-10-06 00:25:55 +0800

    chore: add basic information like name, url, location and so on

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,28 +2,16 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
-  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  name: "Kyler Lam",
+  initials: "KL",
+  url: "https://kyler.github.io",
+  location: "Hong Kong",
+  locationLink: "https://www.google.com/maps/place/hongkong",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "A web development enthusiast with a degree in Software Engineering.",
   summary:
-    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+    "Software Engineering graduate with solid full-stack development skills in Vue3, React, Spring Boot, Flask, and RESTful APIs. Strong programming foundation in Python and Java, with hands-on experience in both independent and collaborative projects. Passionate about LLM-related applications and eager to contribute as a Web Developer / Full-Stack Developer / IT Support.",
   avatarUrl: "/me.jpg",
-  //   "React",
-  //   "Next.js",
-  //   "Typescript",
-  //   "Node.js",
-  //   "Python",
-  //   "Go",
-  //   "Postgres",
-  //   "Docker",
-  //   "Kubernetes",
-  //   "Java",
-  //   "C++",
-  // ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
     { href: "/blog", icon: NotebookIcon, label: "Blog" },

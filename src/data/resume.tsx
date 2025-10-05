de4824cfdee93dbd5c6e804c603ffda600d0bcea
monsterchick <kwanho.app@gmail.com>
2025-10-05 23:45:29 +0800

    refactor: use LinkedIn and Email as main contact methods instead of X

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -34,34 +34,21 @@ export const DATA = {
     social: {
       GitHub: {
         name: "GitHub",
-        url: "https://dub.sh/dillion-github",
+        url: "https://github.com/kylerlam",
         icon: Icons.github,
 
         navbar: true,
       },
       LinkedIn: {
         name: "LinkedIn",
-        url: "https://dub.sh/dillion-linkedin",
+        url: "https://www.linkedin.com/in/kyler123/",
         icon: Icons.linkedin,
 
         navbar: true,
       },
-      X: {
-        name: "X",
-        url: "https://dub.sh/dillion-twitter",
-        icon: Icons.x,
-
-        navbar: true,
-      },
-      Youtube: {
-        name: "Youtube",
-        url: "https://dub.sh/dillion-youtube",
-        icon: Icons.youtube,
-        navbar: true,
-      },
-      email: {
-        name: "Send Email",
-        url: "#",
+      Email: {
+        name: "Email",
+        url: "mailto:kwanho.job@gmail.com",
         icon: Icons.email,
 
         navbar: false,
